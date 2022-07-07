a6e9df7361aac74dfea14c0213c319e88d12c3a8
Linh Vu <linh777gkog@gmail.com>
2022-07-07 23:26:15 +0700

    commit

--- app.js
+++ app.js
@@ -58,3 +58,22 @@ document.addEventListener('DOMContentLoaded', () => {
     [width,width+1,width+2,width*2+1],
     [1,width,width+1,width*2+1]
   ]
+
+   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let currentRotation = 0
+
+  console.log(theTetrominoes[0][0])
+
+  //randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
+
+  //draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+      squares[currentPosition + index].style.backgroundColor = colors[random]
+    })
+  }
