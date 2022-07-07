d0597c14031280c308e0c2ddd128fe91e8918659
Linh Vu <linh777gkog@gmail.com>
2022-07-07 23:25:57 +0700

    commit

--- app.js
+++ app.js
@@ -36,3 +36,25 @@ document.addEventListener('DOMContentLoaded', () => {
     [width,width+1,width+2,width*2+1],
     [1,width,width+1,width*2+1]
   ]
+
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
