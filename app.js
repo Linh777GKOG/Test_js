13c64b6a2d44899e8161e36f54f3df826ea70582
Linh Vu <linh777gkog@gmail.com>
2022-07-07 23:29:07 +0700

    commit

--- app.js
+++ app.js
@@ -31,26 +31,18 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width, width + 1, width * 2 + 1],
   ];
 
-  //The Tetrominoes
-  const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
   ];
 
-  const zTetromino = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-  ];
-
-  const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1],
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
   ];
 
   const theTetrominoes = [
@@ -231,6 +223,7 @@ document.addEventListener('DOMContentLoaded', () => {
         colors[nextRandom];
     });
   }
+
   //add functionality to the button
   startBtn.addEventListener('click', () => {
     if (timerId) {
